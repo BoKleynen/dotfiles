08ebe2f28d13c4245aa1d3ff36e8e35a64aea6b9
Bo Kleynen <kleynenbo@gmail.com>
2022-07-31 20:54:10 +0200

    Add c and go lsp and treesitter

--- config/nvchad/plugins/configs.lua
+++ config/nvchad/plugins/configs.lua
@@ -7,13 +7,18 @@ return {
   },
   ["nvim-treesitter/nvim-treesitter"] = {
     ensure_installed = {
+      "c",
+      "go",
       "haskell",
       "latex",
       "rust",
     },
   },
+  -- HLS is install through ghcup.
   ["williamboman/mason.nvim"] = {
     ensure_installed = {
+      "clangd",
+      "gopls",
       "lua-language-server",
       "rust-analyzer",
     },
