02492c672dcd7e71738044097893ee8be2cfd2e5
Bo Kleynen <kleynenbo@gmail.com>
2023-08-16 09:49:41 +0200

    Remove unused python linter

--- config/nvim/lua/bo/lsp/init.lua
+++ config/nvim/lua/bo/lsp/init.lua
@@ -67,7 +67,6 @@ local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 null_ls.setup {
   sources = {
     null_ls.builtins.formatting.black,
-    null_ls.builtins.diagnostics.mypy,
     null_ls.builtins.diagnostics.ruff,
   },
   on_attach = function(client, bufnr)
