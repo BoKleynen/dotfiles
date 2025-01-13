918974448d9c21bb6b3ecfe979557cb24de29b35
Bo Kleynen <kleynenbo@gmail.com>
2025-01-13 09:00:03 +0100

    Restore atuin defaults

--- home/programs/atuin.nix
+++ home/programs/atuin.nix
@@ -7,6 +7,8 @@
     settings = {
       update_check = false;
       keymap_mode = "vim-insert";
+      style = "auto";
+      inline_height = 0;
       history_filter = [
         "sntrys_"
       ];
