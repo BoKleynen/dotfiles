f338394ceb564cf45d2f364f4b4d29e33656e9a0
Bo Kleynen <kleynenbo@gmail.com>
2025-11-22 08:12:03 +0100

    feat(nvim): add ruff formatter

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -7,6 +7,7 @@ return {
       formatters_by_ft = {
         lua = { "stylua" },
         nix = { "nixfmt" },
+        python = { "ruff_format" },
       },
       format_on_save = {
         -- These options will be passed to conform.format()
